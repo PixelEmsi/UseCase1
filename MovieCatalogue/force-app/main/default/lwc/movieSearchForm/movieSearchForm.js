43286095e5e79460208c6152971db75177b0bded
Amine Bouarraf <amine.bouarraf@gmail.com>
2022-09-22 15:30:03 +0100

    MovieSearch form v1 implementation

---
+++ MovieCatalogue/force-app/main/default/lwc/movieSearchForm/movieSearchForm.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class MovieSearchForm extends LightningElement {
+    selectedMovieId ='';
+    
+    // needs improvement search dynamicaly
+    handleKeyUp(evt) {
+        const isEnterKey = evt.keyCode === 13;
+        if (isEnterKey) {
+            const searchEvent = new CustomEvent('search',{
+                detail : {
+                    boatTypeId: this.selectedMovieId
+                }
+            });
+        this.dispatchEvent(searchEvent);
+        }
+    }
+
+}
